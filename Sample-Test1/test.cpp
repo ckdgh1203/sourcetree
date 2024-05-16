6ea0a68ae7ccff51fbdf5bce2a3ade83b7f94abd
choichangho <ckdgh.choi@samsung.com>
2024-05-16 16:36:56 +0900

    green - test case success

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,6 +1,25 @@
-#include "pch.h"
+﻿#include "pch.h"
+#include "../test/prime-factors.cpp"
+#include <vector>
+using namespace std;
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+class PrimeFixture : public testing::Test {
+public:
+	PrimeFactor prime_factor;
+	vector<int> expected;
+};
+
+TEST_F(PrimeFixture, Of1) {
+	expected = {};
+	EXPECT_EQ(expected, prime_factor.of(1));
+}
+
+TEST_F(PrimeFixture, Of2) {
+	expected = {2};
+	EXPECT_EQ(expected, prime_factor.of(2));
+}
+
+TEST_F(PrimeFixture, Of3) {
+	expected = {3};
+	EXPECT_EQ(expected, prime_factor.of(3));
 }
---
+++ test/prime-factors.cpp
@@ -0,0 +1,16 @@
+﻿#include <vector>
+using namespace std;
+
+class PrimeFactor {
+public:
+	vector<int> of(int number) {
+		vector<int> result = {};
+		if (number == 2) {
+			result.push_back(2);
+		}
+		if (number == 3) {
+			result.push_back(3);
+		}
+		return result;
+	}
+};
