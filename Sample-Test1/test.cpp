50d550f57a8f5c806ac1d5da5452465307f1c9fd
choichangho <ckdgh.choi@samsung.com>
2024-05-16 16:51:02 +0900

    ... of 9 refacot

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -23,3 +23,18 @@ TEST_F(PrimeFixture, Of3) {
 	expected = {3};
 	EXPECT_EQ(expected, prime_factor.of(3));
 }
+
+TEST_F(PrimeFixture, Of4) {
+	expected = {2, 2};
+	EXPECT_EQ(expected, prime_factor.of(4));
+}
+
+TEST_F(PrimeFixture, Of6) {
+	expected = {2, 3};
+	EXPECT_EQ(expected, prime_factor.of(6));
+}
+
+TEST_F(PrimeFixture, Of9) {
+	expected = {3, 3};
+	EXPECT_EQ(expected, prime_factor.of(9));
+}
